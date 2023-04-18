9a087792403d12f6e91310ea739157aea64e0a8c
d.khasenov <d.khasenov@aviata.me>
2023-04-18 08:07:18 +0600

    feat: modal project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React from 'react'
+import './main.css'
 
 function App() {
+  const [open, setOpen] = React.useState(false)
+  const [buttonText, setButtonText] = React.useState('Show me')
+  
+  const handleButton = () => {
+    setOpen(!open)
+  }
+
+  const changeText = () => {
+    setButtonText(open ? 'Hide me' : 'Show me')
+  }
+
+  React.useEffect(() => {
+    changeText()
+  })
+
   return (
     <div className="App">
-      
+      <button onClick={handleButton}>{buttonText}</button>
+      {
+        open && (
+          <div className={`animated ${open && 'show'}`}>
+            Hello world
+          </div>
+        )
+      }
     </div>
   );
 }
