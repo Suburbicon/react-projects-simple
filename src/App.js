4bf791b97bbecce2ec0e6fbe5c38bc75b1519f0e
d.khasenov <d.khasenov@aviata.me>
2023-04-18 07:34:32 +0600

    feat: counter project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [ count, setCount ] = useState(0)
+
+  const onPlus = () => {
+    setCount(count + 1)
+  }
+
+  const onMinus = () => {
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Счетчик</h2>
+        <h1>{ count }</h1>
+        <button onClick={onMinus} className="minus">- минус</button>
+        <button onClick={onPlus} className="plus">+ плюс</button>
+      </div>
     </div>
   );
 }
