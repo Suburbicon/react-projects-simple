e92a512d4018350d06cc73dd1581a66f432fa240
d.khasenov <d.khasenov@aviata.me>
2023-04-23 21:00:05 +0600

    feat: init currency convertor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
   );
 }
